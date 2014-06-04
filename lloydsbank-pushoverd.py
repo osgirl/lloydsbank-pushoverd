fc48ffc614bfd14d568368239752d4b7003eabf4
Tomasz Jan Góralczyk <tomg@fastmail.uk>
2014-06-04 17:39:38 +0100

    Send pushover notifications

--- lloydsbank-pushoverd.py
+++ lloydsbank-pushoverd.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # lloydsbank-pushoverd.py - Receive pushover notification with your Lloyds Bank account balance.
 # This file is part of lloydsbank-pushoverd.
 # Copyright (c) 2014 Tomasz Jan Góralczyk <tomg@fastmail.uk>
@@ -6,11 +7,15 @@
 from __future__ import print_function
 from datetime import date, datetime
 from decimal import Decimal as D
+from hashlib import sha256
+from urllib import urlencode
 import argparse
 import csv
+import httplib
 import json
 import logging
 import mechanize
+import os.path
 import re
 import sys
 
@@ -48,7 +53,8 @@ class Transaction(object):
     }
 
     # Convert CSV data into a transaction object.
-    def __init__(self, fields):
+    def __init__(self, account_name, fields):
+        self.account_name = account_name
         dt = datetime.strptime(fields['Transaction Date'], '%d/%m/%Y')
         self.date = date(dt.year, dt.month, dt.day)
         self.transaction_type = fields['Transaction Type']
@@ -66,6 +72,9 @@ class Transaction(object):
         self.raw_description = fields['Transaction Description']
         self.description = self._parse_description()
 
+        # "Unique" hash of the transaction.
+        self.hash = sha256(self.__repr__()).hexdigest()
+
     def _parse_description(self):
         desc = self.raw_description.strip()
         end_date = self.date.strftime("%d%b%y").upper()
@@ -78,8 +87,8 @@ class Transaction(object):
         return desc
 
     def __repr__(self):
-        return "Transaction(%s, %s, %s, %s)" % (
-                self.date.strftime('%d/%m/%Y'), self.get_type_explanation(),
+        return "Transaction(%s, %s, %s, %s, %s)" % (
+                self.account_name, self.date.strftime('%d/%m/%Y'), self.get_type_explanation(),
                 self.description, self.amount)
 
     def get_type_explanation(self):
@@ -101,6 +110,8 @@ def parse_commandline():
             help='path to the configuration file')
     parser.add_argument('-f', '--force', action='store_true',
             help='send notification even if no new transactions are found')
+    parser.add_argument('--log', default="~/.lloyds_transaction_log",
+            metavar="~/.lloyds_transaction_log", help="file storing the unique ID of last transaction")
 
     # Parse and return arguments.
     return parser.parse_args()
@@ -121,7 +132,7 @@ def parse_configuration(f):
 # password - password
 # memorable_information - memorable information string
 # account_names - Account names.
-def get_account_information(userid, password, memorable_information, account_names):
+def get_account_information(userid, password, memorable_information):
     global USER_AGENT
 
     # Create a browser object.
@@ -161,7 +172,7 @@ def get_account_information(userid, password, memorable_information, account_nam
             accounts.append((re.findall(r'^(.+?)\[IMG\]', link.text)[0], link.absolute_url))
 
     # Fetch transaction list for each account.
-    transaction_logs = []
+    transactions = []
     for account in accounts:
         br.open(account[1])
         export_link = br.find_link(text="Export")
@@ -170,9 +181,52 @@ def get_account_information(userid, password, memorable_information, account_nam
         br.submit()
 
         rows = csv.DictReader(br.response())
-        transaction_logs.append((account[0], [Transaction(row) for row in rows]))
+        transactions += [Transaction(account[0], row) for row in rows]
 
-    return transaction_logs
+    return sorted(transactions, key=lambda t: t.date, reverse=True)
+
+
+# Send a pushover notification if a new transaction is found.
+# force - Send a notification even if no new transactions are found.
+def push_notifications(transactions, app_token, user_key,
+        transaction_log='~/.lloyds_transaction_log', force=False):
+    # Make sure transaction log isn't empty.
+    assert len(transactions) > 0
+
+    # Get hash of last transaction.
+    hash_log = open(os.path.expanduser(transaction_log), 'r+')
+    last_hash = hash_log.read()
+
+    for transaction in transactions:
+        if transaction.hash == last_hash and force is False:
+            break
+        elif transaction.hash == last_hash and force is True:
+            message = "Account balance: £%s" % transaction.balance
+        else:
+            force = False # New transaction found. Don't force.
+            message = "%s %s %s\nAccount balance: £%s" % (transaction.amount,
+                    transaction.get_type_explanation(), transaction.description,
+                    transaction.balance)
+
+        # Send notification.
+        conn = httplib.HTTPSConnection("api.pushover.net:443")
+        conn.request("POST", "/1/messages.json", urlencode({
+            "token": app_token,
+            "user": user_key,
+            "title": transaction.account_name,
+            "message": message
+            }), { "Content-type": "application/x-www-form-urlencoded" })
+        conn.getresponse()
+
+        # Send only one notification if forced.
+        if transaction.hash == last_hash and force is True:
+            break
+
+    # Write last transaction hash to file and close it.
+    hash_log.seek(0)
+    hash_log.write(transactions[0].hash)
+    hash_log.truncate()
+    hash_log.close()
 
 
 def main():
@@ -181,9 +235,12 @@ def main():
     # Load configuration file.
     config = parse_configuration(args.config)
     # Get transaction logs.
-    transaction_logs = get_account_information(config['userid'], config['password'],
-            config['memorable_information'], config['account_names'])
-    print(transaction_logs)
+    transactions = get_account_information(config['userid'], config['password'],
+            config['memorable_information'])
+    # Send notifications.
+    push_notifications(transactions, config['pushover']['token'], config['pushover']['user'],
+            transaction_log=args.log, force=args.force)
+
 
 if __name__ == '__main__':
     main()
